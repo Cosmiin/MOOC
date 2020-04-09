1d7d6e0b5c8148cffc7f87b1eb7e8c867398c4e7
Cosmin <mcosmiin@gmail.com>
2020-04-10 00:24:51 +0300

    second commit

--- src/week1/Exercise13.java
+++ src/week1/Exercise13.java
@@ -1,7 +1,17 @@
 package week1;
 
+import nhlstats.NHLStatistics;
+
 import java.util.Scanner;
 
 public class Exercise13 {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
 
+        System.out.println("Top ten by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.top(10);
+        System.out.println("hello");
+
+    }
 }
